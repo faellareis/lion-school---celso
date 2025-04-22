2f6a1f0cd660a4badd3581d1bf6daab81b5dc5d7
RAFAELLA MARIA TOSCANO REIS <24122319@dsn127m1249387.cfp127aluno.local>
2025-04-22 10:34:56 -0300

    segunda page finalizada

--- app/src/main/java/br/senai/sp/jandira/lionschool/screens/TelaDeCursos.kt
+++ app/src/main/java/br/senai/sp/jandira/lionschool/screens/TelaDeCursos.kt
@@ -1,5 +1,6 @@
 package br.senai.sp.jandira.lionschool.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -13,9 +14,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material3.Card
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
@@ -24,14 +28,18 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.lionschool.R
+
 
 @Composable
 
@@ -97,21 +105,238 @@ fun TelaDeCurso(modifier: Modifier = Modifier) {
                     )
                 },
                 modifier = Modifier
-                    .fillMaxWidth(0.85f)
-                    .padding(bottom = 16.dp)
-                    .align(Alignment.CenterHorizontally),
-                shape = RoundedCornerShape(16.dp),
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    focusedBorderColor = Color(0xFF3347B0),
-                    unfocusedBorderColor = Color(0xFFF9B036)
-                )
+                    .padding(15.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(12.dp),
             )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(top = 2.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.courses),
+                    contentDescription = " ",
+                    modifier = Modifier.size(30.dp)
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = stringResource(R.string.courses),
+                    fontSize = 20.sp,
+                    color = Color(0xFF3347B0),
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Column {
+                Card(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth()
+                        .height(180.dp),
+                    shape = RoundedCornerShape(12.dp),
+                    border = BorderStroke(
+                        width = 0.dp,
+                        color = Color(0xFFF9B036)
+                    )
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(start = 15.dp, bottom = 8.dp),
+                        horizontalAlignment = Alignment.Start //esquerda
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Start
+                        ) {
+                            Image(
+                                painter = painterResource(R.drawable.ds),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .size(100.dp),
+                                contentScale = ContentScale.Fit
+                            )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text(
+                                text = stringResource(R.string.ds),
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 60.sp,
+                                color = Color(0xFFF9B036)
+                            )
+                        }
+                        Text(
+                            text = stringResource(R.string.ds2),
+                            fontSize = 15.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color(0xFFFFFFFF)
+                        )
+                        Text(
+                            modifier = Modifier
+                                .padding(top = 5.dp),
+                            text = stringResource(R.string.texto_ds),
+                            fontSize = 12.sp,
+                            color = Color(0xFFFFFFFF)
+                        )
+                        Row(
+                            modifier = Modifier
+                                .padding(top = 15.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.relogio),
+                                contentDescription = "",
+                                modifier = Modifier.size(15.dp)
+                            )
+                            Spacer(modifier = Modifier.width(3.dp))
+                            Text(
+                                text = stringResource(R.string.semestres),
+                                fontSize = 10.sp,
+                                color = Color(0xFFFFFFFF)
+                            )
+                        }
+                    }
+                }
+                Card(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth()
+                        .height(180.dp),
+                    shape = RoundedCornerShape(12.dp),
+                    border = BorderStroke(
+                        width = 0.dp,
+                        color = Color(0xFFF9B036)
+                    )
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(start = 15.dp, bottom = 8.dp),
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Start
+                        ) {
+                            Image(
+                                painter = painterResource(R.drawable.rds),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .size(100.dp),
+                                contentScale = ContentScale.Fit
+                            )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text(
+                                text = stringResource(R.string.rds),
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 60.sp,
+                                color = Color(0xFFF9B036)
+                            )
+                        }
+                        Text(
+                            text = stringResource(R.string.rds2),
+                            fontSize = 15.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color(0xFFFFFFFF)
+                        )
+                        Text(
+                            modifier = Modifier
+                                .padding(top = 5.dp),
+                            text = stringResource(R.string.texto_ds),
+                            fontSize = 12.sp,
+                            color = Color(0xFFFFFFFF)
+                        )
+                        Row(
+                            modifier = Modifier
+                                .padding(top = 15.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.relogio),
+                                contentDescription = "",
+                                modifier = Modifier.size(15.dp)
+                            )
+                            Spacer(modifier = Modifier.width(3.dp))
+                            Text(
+                                text = stringResource(R.string.semestres),
+                                fontSize = 10.sp,
+                                color = Color(0xFFFFFFFF)
+                            )
+                        }
+                    }
+                }
+                Card(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth()
+                        .height(180.dp),
+                    shape = RoundedCornerShape(12.dp),
+                    border = BorderStroke(
+                        width = 0.dp,
+                        color = Color(0xFFF9B036)
+                    )
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(start = 15.dp, bottom = 8.dp),
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Start
+                        ) {
+                            Image(
+                                painter = painterResource(R.drawable.ele),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .size(100.dp),
+                                contentScale = ContentScale.Fit
+                            )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text(
+                                text = stringResource(R.string.ele),
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 60.sp,
+                                color = Color(0xFFF9B036)
+                            )
+                        }
+                        Text(
+                            text = stringResource(R.string.ele2),
+                            fontSize = 15.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color(0xFFFFFFFF)
+                        )
+                        Text(
+                            modifier = Modifier
+                                .padding(top = 5.dp),
+                            text = stringResource(R.string.texto_rds),
+                            fontSize = 12.sp,
+                            color = Color(0xFFFFFFFF)
+                        )
+                        Row(
+                            modifier = Modifier
+                                .padding(top = 15.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.relogio),
+                                contentDescription = "",
+                                modifier = Modifier.size(15.dp)
+                            )
+                            Spacer(modifier = Modifier.width(3.dp))
+                            Text(
+                                text = stringResource(R.string.semestres),
+                                fontSize = 10.sp,
+                                color = Color(0xFFFFFFFF)
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
-
-
-
 
 @Preview(showSystemUi = true)
 @Composable
