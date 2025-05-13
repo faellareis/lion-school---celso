3125958b9bb5de0caea372644edc5d48905d3086
RAFAELLA MARIA TOSCANO REIS <24122319@dsn127m1249387.cfp127aluno.local>
2025-05-13 12:26:56 -0300

    3 ok

--- app/src/main/java/br/senai/sp/jandira/lionschool/screens/TelaDeLista.kt
+++ app/src/main/java/br/senai/sp/jandira/lionschool/screens/TelaDeLista.kt
@@ -1,6 +1,5 @@
 package br.senai.sp.jandira.lionschool.screens
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -8,7 +7,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -34,7 +33,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -53,7 +51,6 @@ fun TelaDeLista(modifier: Modifier = Modifier) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxSize()
                 .padding(horizontal = 16.dp, vertical = 16.dp)
         ) {
             Row(
@@ -114,23 +111,24 @@ fun TelaDeLista(modifier: Modifier = Modifier) {
             OutlinedTextField(
                 value = "",
                 onValueChange = {},
-                label = { Text(stringResource(R.string.find_student)) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
+                shape = RoundedCornerShape(10.dp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedBorderColor = Color(0xFFFFC23D),
+                    focusedBorderColor = Color(0xFFFFC23D)
+                ),
+                label = {
+                    Text(text = stringResource(R.string.find_student))
+                },
                 trailingIcon = {
                     Icon(
                         imageVector = Icons.Default.Search,
-                        contentDescription = null,
+                        contentDescription = "",
                         tint = Color.Gray
                     )
                 },
-                modifier = Modifier
-                    .padding(15.dp)
-                    .align(Alignment.CenterHorizontally)
-                    .fillMaxWidth(),
-                shape = RoundedCornerShape(12.dp),
-                colors = OutlinedTextFieldDefaults.colors(
-                    unfocusedBorderColor = Color(0xFFF9B036),
-                    focusedBorderColor = Color(0xFFF9B036)
-                )
             )
             Row(
                 modifier = Modifier
@@ -139,49 +137,53 @@ fun TelaDeLista(modifier: Modifier = Modifier) {
                 horizontalArrangement = Arrangement.SpaceAround
             ) {
                 Button(
-                    onClick = { },
+                    onClick = {},
                     shape = RoundedCornerShape(90.dp),
                     colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(0xFF3347B0)
-                    )
-                ) {
-                    Text(stringResource(R.string.todos))
-                }
-                Button(
-                    onClick = { },
-                    shape = RoundedCornerShape(90.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(0xFFF9B036)
+                        containerColor = Color(0XFF3347B0)
                     )
                 ) {
                     Text(
-                        stringResource(R.string.cursando),
-                        color = Color.White
+                        text = stringResource(R.string.todos),
+                        color = Color(0xF5FAFAFA)
                     )
                 }
                 Button(
-                    onClick = { },
+                    onClick = {},
                     shape = RoundedCornerShape(90.dp),
                     colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(0xFFF9B036)
+                        containerColor = Color(0XFFFFC23D)
                     )
                 ) {
                     Text(
-                        stringResource(R.string.finalizado),
-                        color = Color.White
+                        text = stringResource(R.string.cursando),
+                        color = Color(0XFF3347B0)
+                    )
+                }
+                Button(
+                    onClick = {},
+                    shape = RoundedCornerShape(90.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0XFFFFC23D)
+                    )
+                ) {
+                    Text(
+                        text = stringResource(R.string.finalizado),
+                        color = Color(0XFF3347B0)
                     )
                 }
             }
             Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(bottom = 3.dp)
+                modifier = Modifier
+                    .padding(top = 10.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.student),
-                    contentDescription = " ",
-                    modifier = Modifier.size(30.dp)
+                    contentDescription = " list ",
+                    modifier = Modifier
+                        .size(30.dp)
                 )
-                Spacer(modifier = Modifier.width(5.dp))
                 Text(
                     text = stringResource(R.string.student_list),
                     fontSize = 20.sp,
@@ -189,91 +191,49 @@ fun TelaDeLista(modifier: Modifier = Modifier) {
                     fontWeight = FontWeight.Bold
                 )
             }
-            Box(
-                modifier = Modifier
-                    .height(175.dp)
-                    .fillMaxWidth()
-                    .padding((10.dp))
-            ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .width(20.dp)
-                        .background(
-                            Color(0xFFF9B036)
-                        )
-                )
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 8.dp, vertical = 4.dp)
-                        .height(70.dp),
-                    shape = RoundedCornerShape(8.dp),
-                    colors = CardDefaults.cardColors(
-                        containerColor = Color(0xFF9FA9E1)
-                    )
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .width(10.dp)
-                            .height(80.dp)
-                            .background(Color(0xFFF9B036))
-                    )
-                }
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.padding(8.dp)
-                ) {
-
-                    Spacer(modifier = Modifier.width(16.dp))
-                    Image(
-                        painter = painterResource(R.drawable.luana),
-                        contentDescription = stringResource(R.string.luana),
-                        modifier = Modifier
-                            .size(50.dp)
-                            .clip(CircleShape),
-                        contentScale = ContentScale.Crop
-                    )
-                    Spacer(modifier = Modifier.width(16.dp))
-                    Column(modifier = Modifier.weight(1f)) {
-                        Text(
-                            text = stringResource(R.string.luana),
-                            fontWeight = FontWeight.SemiBold,
-                            fontSize = 18.sp,
-                            color = Color.White
-                        )
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.identification),
-                                contentDescription = stringResource(R.string.numero_estudante),
-                                tint = Color.White,
-                                modifier = Modifier.size(14.dp)
-                            )
-                            Spacer(modifier = Modifier.width(4.dp))
-                            Text(
-                                text = stringResource(R.string.numero_estudante),
-                                color = Color.White,
-                                fontSize = 10.sp
-                            )
+            Spacer(modifier = Modifier.height(10.dp))
+            LazyColumn {
+                items(7) { index ->
+                    StudentCard(
+                        studentName = when (index) {
+                            0 -> stringResource(R.string.luana)
+                            1 -> stringResource(R.string.pedro)
+                            2 -> stringResource(R.string.ana_clara)
+                            3 -> stringResource(R.string.fabiana_luz)
+                            4 -> stringResource(R.string.carlos_augusto)
+                            5 -> stringResource(R.string.pedro_xavier)
+                            else -> ""
+                        },
+                        studentCode = when (index) {
+                            0 -> "20151001018"
+                            1 -> "20151001018"
+                            2 -> "20151001018"
+                            3 -> "20151001018"
+                            4 -> "20151001018"
+                            5 -> "20151001018"
+                            else -> ""
+                        },
+                        graduationYear = when (index) {
+                            0 -> "2022"
+                            1 -> "2022"
+                            2 -> "2025"
+                            3 -> "2022"
+                            4 -> "2025"
+                            5 -> "2025"
+                            else -> ""
+                        },
+                        profileImage = when (index) {
+                            0 -> R.drawable.luana
+                            1 -> R.drawable.pedrosilva
+                            2 -> R.drawable.anaclara
+                            3 -> R.drawable.fabiana_luz
+                            4 -> R.drawable.carlos
+                            5 -> R.drawable.pedroxavier
+                            else -> R.drawable.pedroxavier
                         }
-                    }
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.calendario),
-                            contentDescription = " ",
-                            modifier = Modifier.size(15.dp)
-                        )
-                        Spacer(modifier = Modifier.width(5.dp))
-                        Text(
-                            text = stringResource(R.string.doismildoisdois),
-                            fontSize = 10.sp,
-                            color = Color(0xFFF9B036),
-                            fontWeight = FontWeight.ExtraBold
-                        )
+                    )
+                    if (index < 6) {
+                        Spacer(modifier = Modifier.height(8.dp))
                     }
                 }
             }
@@ -281,6 +241,107 @@ fun TelaDeLista(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun StudentCard(
+    studentName: String,
+    studentCode: String,
+    graduationYear: String,
+    profileImage: Int
+) {
+    Box(
+        modifier = Modifier
+            .height(85.dp)
+            .padding(vertical = 4.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .width(80.dp)
+                .height(85.dp)
+                .background(
+                    Color(0xFFFFC23D),
+                    RoundedCornerShape(topStart = 12.dp, bottomStart = 12.dp)
+                )
+        )
+        // Card principal
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(85.dp)
+                .padding(start = 8.dp),
+            shape = RoundedCornerShape(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color(0xFFACB1E5)
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(start = 12.dp, top = 12.dp, end = 12.dp, bottom = 8.dp),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .size(45.dp)
+                            .clip(CircleShape),
+                        painter = painterResource(profileImage),
+                        contentDescription = "",
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Column {
+                        Text(
+                            text = studentName,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp,
+                            color = Color.White
+                        )
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                painter = painterResource(R.drawable.identification),
+                                contentDescription = "",
+                                tint = Color.White,
+                                modifier = Modifier.size(12.dp)
+                            )
+                            Spacer(modifier = Modifier.width(3.dp))
+                            Text(
+                                text = studentCode,
+                                fontSize = 12.sp,
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.calendario),
+                        contentDescription = "",
+                        tint = Color.White,
+                        modifier = Modifier.size(12.dp)
+                    )
+                    Spacer(modifier = Modifier.width(3.dp))
+                    Box(
+                        modifier = Modifier
+                            .padding(horizontal = 6.dp, vertical = 1.dp)
+                    ) {
+                        Text(
+                            text = graduationYear,
+                            fontSize = 10.sp,
+                            color = Color(0xFFFFC23D),
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
 
 @Preview(showSystemUi = true)
 @Composable
